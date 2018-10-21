9e7cd190cee6a20bc8d4149825de83a94e9da54e
GeGe-K <gloriagivondo@gmail.com>
2018-10-21 20:54:37 +0300

    updated the quote class and added a showDesciption property set to boolean value.

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,3 +1,6 @@
 export class Quote {
-  
+  public showDescription:boolean;
+  constructor(public name:string, public author:string, public publisher:string) {
+    this.showDescription=false;
+  }
 }
