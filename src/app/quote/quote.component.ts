84e23c60bc56b75f90d6400cbfcbf04233b1694c
GeGe-K <gloriagivondo@gmail.com>
2018-10-21 22:24:41 +0300

    added a toogleDetails button.

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,11 +7,14 @@ import { Quote } from "../quote"
 })
 export class QuoteComponent implements OnInit {
   quotes=[
-    new Quote('Live one day at a tome emphasizing ethics rather than rules', 'Wayne Dyre'),
-    new Quote('Obstacles are those frightful things you see when you take your eyes off your goal.','Theodore Rossevelt')
+    new Quote('Live one day at a tome emphasizing ethics rather than rules', 'Wayne Dyre','Gloria'),
+    new Quote('Obstacles are those frightful things you see when you take your eyes off your goal.','Theodore Rossevelt','Jane')
   ]
+  toogleDetails(index){
+    this.quotes[index].showDescription = !this.quotes[index].showDescription;
+  }
   constructor() { }
- 
+
   ngOnInit() {
   }
 
