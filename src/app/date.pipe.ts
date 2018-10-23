f72924228f79c8574536a3a015c51e1c88cda7a8
GeGe-K <gloriagivondo@gmail.com>
2018-10-23 04:57:19 +0300

    added comments to the date pipe.

--- src/app/date.pipe.ts
+++ src/app/date.pipe.ts
@@ -1,13 +1,13 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'date'
+  name: 'lapse'
 })
 export class DatePipe implements PipeTransform {
 
   transform(value: any): any {
-    let timeNow = new Date();
-    let difference = timeNow.getTime() - value.getTime();
+    let timeNow = new Date(); //Takes the current date
+    let difference = timeNow.getTime() - value.getTime(); //Difference btwn current date and when it was posted
     difference = difference / (1000 * 60 * 60 * 24);
     return Math.floor(difference);
   }
